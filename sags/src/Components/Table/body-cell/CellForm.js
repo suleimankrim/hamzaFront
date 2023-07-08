e8acf615bb040e372984b64864cac8202c87abc0
Hamza Shafee Aldaghstany <118183676+Hamza-Shafee-Aldaghstany@users.noreply.github.com>
2023-07-08 03:55:10 +0300

    createTeam

--- sags/src/Components/Table/body-cell/CellForm.js
+++ sags/src/Components/Table/body-cell/CellForm.js
@@ -2,10 +2,12 @@ import React from "react";
 import Style from "./Cell.module.css";
 import { useFormik } from "formik";
 import secondStyle from "./IdCell.module.css";
-import {data} from "../MockData";
+import {data, set} from "../MockData";
 import axios from "axios";
 import TeamObserver from "../../TeamsObserver";
+import  { useState } from 'react';
 export const CellForm = ({ row, index , settingsStates }) => {
+  const [isFocused, setIsFocused] = useState(false);
   let observer=null
   console.log(data[row.id-data[1].id]);
   console.log(row.id-data[1].id);
@@ -36,15 +38,32 @@ export const CellForm = ({ row, index , settingsStates }) => {
   const forceChange=(e)=>{
     formik.values.cellValue=e;
   }
+    const onFocusHandler=(e)=>{
+        if(settingsStates.getCreateTeam()===true){
+          setcstyle(Style.cell2);
+        }
+      
+    }
+  
+  
+  const[cstyle,setcstyle] =useState(Style.cell)
+  // isFocused?  Style.cell2: Style.cell;
+  //  const inputClassName2=index === 0 ? secondStyle.IdCell:inputClassName;
   return (
     
     <input
+      // className={'highlighted'}
       type="text"
       name="cellValue"
       id="cellValue"
       value={formik.values.cellValue}
       onChange={change }
+      onFocus={onFocusHandler}
       onBlur={async () => {
+        if(settingsStates.getCreateTeam()===true){
+          
+        }
+        else{
         row[filds[index]] = formik.values.cellValue;
         console.log(row.id);
         try {
@@ -58,7 +77,7 @@ export const CellForm = ({ row, index , settingsStates }) => {
           console.error(error);
           // Handle the error or throw it for further handling
         }
-      }}
+      }}}
       // In the code above, you can include the necessary key-value pairs inside the object passed as the second argument to axios.put. Replace key1, value1, key2, value2, etc. with the actual data you want to send in the JSON body.
       
       // Ensure that the server-side API endpoint expects and can handle the JSON data being sent in the request body.
@@ -69,7 +88,7 @@ export const CellForm = ({ row, index , settingsStates }) => {
       
       
       
-      className={index === 0 ? secondStyle.IdCell : Style.cell}
+      className={cstyle}
     ></input>
   );
 };
